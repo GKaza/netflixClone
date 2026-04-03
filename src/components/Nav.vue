8de6f221004b4cb2e4d14f8bd015cfd2465dac6f
Giannis Kazakidis <46610719+GKaza@users.noreply.github.com>
2026-04-03 21:15:50 +0300

    update logo

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="nav" :class="{ nav_black: show }">
     <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1024px-Netflix_2015_logo.svg.png"
+      src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg"
       alt="Netflix Logo"
       class="nav_logo"
     />
